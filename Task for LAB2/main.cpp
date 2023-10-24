d35351c561cab44945dbc1166f9f6023b23bf9a9
DizzyWaters <mkulvanovskis@gmail.com>
2023-10-24 12:26:51 +0300

    added checking denominator

--- Task for LAB2/main.cpp
+++ Task for LAB2/main.cpp
@@ -2,8 +2,6 @@
 
 #include <iostream>
 #include <cmath>
-#include <cstdlib>
-#include <climits>
 
 using namespace std;
 
@@ -18,19 +16,38 @@ int main()
 
 	// task nr.15
 
-	double userInput;
+	double arr[] = { -7834, -2 , 0 , 2 , 4387 };
+	int input;
 	double z1;
 	double z2;
+	double denominator;
 
-	cout << "\nPlease input value: ";
-	cin >> userInput;
-	cout << "\n" << userInput << endl;
-	z1 = sqrt((2 * userInput) + (2 * sqrt(pow(userInput, 2) - 4))) / (sqrt(pow(userInput, 2) - 4) + userInput + 2);
-	cout << "z1 is: " << z1 << endl;
+	// to not get c2124 
+	for (int i = 0; i < 5; i++) 
+	{
+		input = arr[i];
+		cout << "\ninput value is: " << arr[i];
+		
+		denominator = (sqrt(pow(input, 2) - 4) + input + 2);
+		
+		if (denominator == 0)
+			cout << "\nExeption: Can not divide by zero." << endl;
+		else
+		{
+			z1 = sqrt((2 * input) + (2 * sqrt(pow(input, 2) - 4))) / denominator;
+			cout << "\nz1 is: " << z1 << endl;
+		}
 
-	z2 = sqrt((1.0)/ (userInput + 2));
-	cout << "z2 is: " << z2 << endl;
-
+		denominator = sqrt(input + 2);
+		
+		if (denominator == 0)
+			cout << "Exeption: Can not divide by zero.\n";
+		else 
+		{
+			z2 = (sqrt(1) / denominator);
+			cout << "z2 is: " << z2 << endl;
+		}
+	}
 
 
 
